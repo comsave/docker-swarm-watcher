267533f4ab6f4c803dd72e0a63a0df91b37b35a0
Joeri Veder <joeri.veder@comsave.com>
2018-06-20 15:50:12 +0200

    added flag to define accesible port

--- main.go
+++ main.go
@@ -4,16 +4,18 @@ import (
 	"log"
 	"net/http"
 	"flag"
+	"fmt"
 )
 
 var (
-	command  = flag.String("c", "docker-gen", "Command to execute when an event is fired")
+	command  = flag.String("c", "/bin/echo", "Command to execute when an event is fired")
 	username = flag.String("u", "username", "Basic authentication username")
 	password = flag.String("p", "password", "Basic authentication password")
+	port     = flag.String("port", "8888", "Port to expose")
 )
 
 func main() {
 	flag.Parse()
 
-	log.Fatal(http.ListenAndServe(":8888", NewRouter(routes)))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s",*port), NewRouter(routes)))
 }
