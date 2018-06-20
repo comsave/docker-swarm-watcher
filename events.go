5ad2f9e895be6831056089fc27ab92fc4f3ce81a
Joeri Veder <joeri.veder@comsave.com>
2018-06-20 17:11:23 +0200

    re-added service name

--- events.go
+++ events.go
@@ -4,11 +4,13 @@ import (
 	"net/http"
 	"os/exec"
 	"os"
+	"fmt"
 )
 
 func eventFired(w http.ResponseWriter, r *http.Request) {
+	var serviceName = r.URL.Query().Get("serviceName")
 
-	_, err := exec.Command("/bin/bash", "-c", *command).CombinedOutput()
+	_, err := exec.Command("/bin/bash", "-c", fmt.Sprintf("%s %s",*command, serviceName)).CombinedOutput()
 
 	if err != nil {
 		os.Stderr.WriteString(err.Error())
